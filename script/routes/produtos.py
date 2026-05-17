6c9101fa8d4fb04f6378989d300076efda231401
Vininfilho <viniciusnunes.r.filho@gmail.com>
2026-05-16 23:34:54 -0300

    Removido duplicado

--- script/routes/produtos.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from script.utils.bd import supabase
-
-produtos_bp = Blueprint('produtos', __name__)
-
-@produtos_bp.route('/api/produtos', methods=['GET'])
-def listar_produtos_api():
-    response = supabase.table('products').select('*').execute()
-    return jsonify(response.data)
