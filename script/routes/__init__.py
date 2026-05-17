1f673527459282bd0cc7e0a753e43bf181fa5452
Vininfilho <viniciusnunes.r.filho@gmail.com>
2026-05-16 23:33:37 -0300

    Decrição produto

--- script/routes/__init__.py
+++ script/routes/__init__.py
@@ -1,12 +1,10 @@
 from script.routes.login import login_bp
 from script.routes.logout import logout_bp
 from script.routes.register import register_bp
-from script.routes.produtos import produtos_bp
 from script.routes.backoffice import backoffice_bp  
 
 def register_blueprints(app):
     app.register_blueprint(login_bp)
     app.register_blueprint(logout_bp)
     app.register_blueprint(register_bp)
-    app.register_blueprint(produtos_bp)   # agora está consistente
     app.register_blueprint(backoffice_bp)
